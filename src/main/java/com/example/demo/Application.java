f9584d5172223209c2bdbf4ce00fbbed0312fe23
David Lin <david11yf29@gmail.com>
2025-03-17 20:10:44 +0800

    2025.03.17 Jpa learning2

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -1,11 +1,10 @@
 package com.example.demo;
 
+import com.github.javafaker.Faker;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
-import java.util.List;
 
 @SpringBootApplication
 public class Application {
@@ -17,28 +16,18 @@ public class Application {
     @Bean
     CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
         return args -> {
-            Student maria = new Student(
-                    "Maria", "Jones", "maria.jones@email.com", 21
-            );
-
-            Student maria2 = new Student(
-                    "Maria", "Jones", "maria2.jones@email.com", 25
-            );
-
-            Student ahmed = new Student(
-                    "Ahmed", "Ali", "ahmed.ali@email.com", 18
-            );
-
-            studentRepository.saveAll(List.of(maria, ahmed, maria2));
-
-            studentRepository
-                    .findStudentByEmail("ahmed.ali@email.com")
-                    .ifPresentOrElse(System.out::println,
-                            () -> System.out.println("Student with email ahmed.ali@email.com not found"));
-
-            studentRepository
-                    .findStudentsByFirstNameEqualsAndAgeIsGreaterThanEqual("Maria", 18)
-                    .forEach(System.out::println);
+            Faker faker = new Faker();
+            for (int i = 0; i < 20 ; i++) {
+                String firstName = faker.name().firstName();
+                String lastName = faker.name().lastName();
+                String email = String.format("%s.%s@gmail.com", firstName, lastName);
+                Student student = new Student(
+                        firstName,
+                        lastName,
+                        email,
+                        faker.number().numberBetween(17, 55));
+                studentRepository.save(student);
+            }
         };
     }
 
--- src/main/java/com/example/demo/StudentRepository.java
+++ src/main/java/com/example/demo/StudentRepository.java
@@ -1,11 +1,24 @@
 package com.example.demo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
+	@Query("SELECT s FROM Student s WHERE s.email = ?1")
 	Optional<Student> findStudentByEmail(String email);
-	List<Student> findStudentsByFirstNameEqualsAndAgeIsGreaterThanEqual(String firstName, Integer age);
+
+	@Query("SELECT s FROM Student s WHERE s.firstName = :firstName AND s.age >= :age")
+	List<Student> findStudentsByFirstNameEqualsAndAgeIsGreaterThanEqual(
+			@Param("firstName") String firstName,@Param("age") Integer age);
+
+	@Transactional
+	@Modifying
+	@Query("DELETE FROM Student u WHERE u.id = ?1")
+	int deleteStudentById(Long id);
 }
